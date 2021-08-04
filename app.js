5b91086bb5fa60333cf8745770812d126ad0b9b7
RyanThahir <ryant.thahir@gmail.com>
2021-08-04 10:58:47 +0800

    disclaimer button + modal is fixed

--- app.js
+++ app.js
@@ -21,20 +21,18 @@ window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
     
-    opacity.forEach(element => {
-        element.style.opacity = (scroll / (sectionY.top + section_height))+0.5;
-    })
+    
     let speedbg = bg.dataset.speed;
-    bg.style.transform = `translateY(${scroll*speedbg}px)`
+    bg.style.transform = `translateY(${scroll*speedbg*1.5}px)`
 	
 	let speedbam1 = bam1.dataset.speed;
-	bam1.style.transform = `translateX(${scroll*speedbam1*(-1)}px`;
+	bam1.style.transform = `translateX(${scroll*speedbam1*(-2)}px`;
 	let speedbam2 = bam2.dataset.speed;
-	bam2.style.transform = `translateX(${scroll*speedbam2*(-1)}px`;
+	bam2.style.transform = `translateX(${scroll*speedbam2*(-2)}px`;
 	let speedpa = peoplea.dataset.speed;
-	peoplea.style.transform = `translateX(${scroll*speedpa*(-1)}px`;
+	peoplea.style.transform = `translateX(${scroll*speedpa*(-2)}px`;
 	let speedpi = peoplei.dataset.speed;
-	peoplei.style.transform = `translateX(${scroll*speedpi}px`;
+	peoplei.style.transform = `translateX(${scroll*speedpi*2}px`;
     // let speedbg = bg.dataset.speed
     // bg.style.transform = `translateY(${scroll * speedbg}px)`
 
@@ -43,7 +41,7 @@ window.addEventListener('scroll', () => {
     shadow.style.height = `${scroll * 0.5 + 50}px`;
 
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`
-    image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`
+    
 
     // border.style.width = `${scroll / (sectionY.top + section_height) * 50}%`
 
@@ -58,7 +56,27 @@ window.addEventListener('scroll', () => {
     }
 
 })
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
